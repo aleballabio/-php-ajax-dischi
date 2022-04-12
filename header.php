62b933a6c7b8938bd0e3c830d1cddd6889ef25e2
aleballabio <aleballabio05@gmail.com>
2022-04-12 19:16:24 +0200

    setted data and everything

---
+++ header.php
@@ -0,0 +1,5 @@
+<header class="container-fluid">
+    <div class="cont-obj">
+        <img src="img/logo-small.svg" alt="Logo" class="logo" />
+    </div>
+</header>
---
+++ main.php
@@ -0,0 +1,14 @@
+<main>
+    <div class="container-fluid">
+        <div class="container-cards">
+            <div class="card">
+                <div class="subcard">
+                    <img :src="" :alt="" />
+                    <h1></h1>
+                    <h2></h2>
+                    <h3></h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
